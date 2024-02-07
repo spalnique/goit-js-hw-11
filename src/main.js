1304a60b4eb989b0b0b0ec4f0195c6bf1059bf65
Oleksii_S <lex.sydoruk@gmail.com>
2024-02-07 21:34:40 +0100

    .

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import rejectedIcon from './img/rejectedIcon.svg';
+import closeIcon from './img/izitoast-close.svg';
 
 class LoaderSpinner {
   #previousHTML;
+
   constructor(parentElemQuery = '') {
     this.parent = parentElemQuery;
     this.#previousHTML = '';
@@ -42,6 +45,7 @@ class Gallery {
       rest.forEach(keyName => {
         filtered[keyName] = obj[keyName];
       });
+      console.log(filtered);
       return filtered;
     });
   }
@@ -68,13 +72,7 @@ class Gallery {
   }
 }
 
-// obj.largeImageURL,
-//   obj.webformatURL,
-//   obj.alt,
-//   obj.likes,
-//   obj.views,
-//   obj.comments,
-//   obj.downloads;
+//////////////////////////////////////////////////////////////////////////////////////////
 
 const refs = {
   form: document.querySelector('.js-search-form'),
@@ -82,23 +80,61 @@ const refs = {
   container: document.querySelector('.js-gallery'),
 };
 
+const requestUrl = 'https://pixabay.com/api/';
+
+const requestParams = {
+  key: '42242477-df8643eaa45736c853493b589',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: true,
+  q: null,
+};
+
+const spinner = new LoaderSpinner('.js-gallery');
+
+const iziOptions = {
+  class: 'js-izitoast-message',
+  titleColor: '#FFFFFF',
+  message: null,
+  messageColor: '#FFFFFF',
+  messageSize: '16px',
+  position: 'topRight',
+  backgroundColor: '#EF4040',
+  progressBarColor: '#B51B1B',
+  iconUrl: rejectedIcon,
+  close: false,
+  buttons: [
+    [
+      `<button type="button" style="background-color: transparent;"><img src=${closeIcon}></button>`,
+      function (instance, toast) {
+        instance.hide({ transitionOut: 'fadeOut' }, toast);
+      },
+    ],
+  ],
+  onOpening: function (instance, toast) {
+    refs.container.innerHTML = '';
+    refs.input.blur();
+    refs.input.addEventListener(
+      'focus',
+      () => {
+        iziToast.hide({ transitionOut: 'fadeOut' }, toast);
+      },
+      { once: true }
+    );
+  },
+};
+
 refs.form.addEventListener('submit', e => {
   e.preventDefault();
+  requestParams.q = refs.input.value.trim();
 
-  if (!Gallery.testInput(refs.input.value)) {
-    iziToast.error({}); // add options obj
+  if (!Gallery.testInput(refs.input.value.trim())) {
+    (iziOptions.message =
+      'Try something like "kitty", "best friends", "on the Moon" ;)'),
+      iziToast.show(iziOptions);
+    refs.form.reset();
     return;
   }
-
-  const requestUrl = 'https://pixabay.com/api/';
-  const requestParams = {
-    key: '42242477-df8643eaa45736c853493b589',
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    q: refs.input.value.trim(),
-  };
-  const spinner = new LoaderSpinner('.js-gallery');
 
   fetch(`${requestUrl}?${new URLSearchParams(requestParams)}`)
     .then(response => {
@@ -110,7 +146,10 @@ refs.form.addEventListener('submit', e => {
     })
     .then(data => {
       if (!data.hits.length) {
-        iziToast.show(iziOptions);
+        (iziOptions.message =
+          'Sorry, there are no images matching your search query. Please try again!'),
+          iziToast.show(iziOptions);
+        refs.form.reset();
         return;
       }
       const lightboxInstance = new SimpleLightbox('.js-gallery a', {
